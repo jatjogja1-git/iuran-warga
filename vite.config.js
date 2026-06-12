d512511ec8d0000881f4f026960c4517415f78d5
Jat Jogja <jatjogja1@gmail.com>
2026-06-12 19:44:28 +0700

    Menambahkan fitur qrcode histori pembayaran di tabel warga

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+//import path from 'path' //defautlnya tidak ada
 
+// ini default nya
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
 })
+
+
+/* untuk localhost
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    host: '192.168.4.140',
+    port: 5199,
+    strictPort: true, 
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
+*/
